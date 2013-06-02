afcfa6cd7954d1b4ad816c42e0e04299c88d418c
Richard A (rical) <ric41@yahoo.se>
2013-06-02 11:51:45 +0200

    Add ssh functionality
    It might still need some polish and a proper unit test.
    
    example.tcl
    -----------
    shell "localhost"
    
    ssh "machine"
    if {[execute hostname] == "machine"} {
        result OK "Connecting to $host"
    }
    
    config.yaml
    -----------
    machine:
        SSH_IP: 10.0.0.1
        SSH_PORT: 1234
        PROMPT: "@machine"
    
    Signed-off-by: Richard A (rical) <ric41@yahoo.se>

--- pkgAll.tcl
+++ pkgAll.tcl
@@ -6,5 +6,6 @@ package require 9pm::config 1.0
 package require 9pm::init 1.0
 package require 9pm::shell 1.0
 package require 9pm::execute 1.0
+package require 9pm::ssh 1.0
 
 package provide 9pm 1.0
---
+++ ssh.tcl
@@ -0,0 +1,39 @@
+package provide 9pm::ssh 1.0
+
+proc ssh {node args} {
+    set IP     [get_req_node_info $node SSH_IP]
+    set PROMPT [get_req_node_info $node PROMPT]
+    set PORT   [get_node_info $node SSH_PORT]
+    set USER   [get_node_info $node SSH_USER]
+    set PASS   [get_node_info $node SSH_PASS]
+
+    set opts "-o StrictHostKeyChecking=no"
+
+    set ssh_cmd "ssh $opts $IP"
+    if {$USER != ""} {
+        append ssh_cmd " -l $USER"
+    }
+    if {$PORT != ""} {
+        append ssh_cmd " -p $PORT"
+    }
+
+    output DEBUG "Connecting to \"$IP\" (as \"$USER\")"
+
+    expect *
+    send "$ssh_cmd\n"
+    expect {
+        $PROMPT {
+            output INFO "Connected to \"$IP\" (as \"$USER\")"
+        }
+        -nocase "password" {
+            send "$PASS\n"
+            exp_continue -continue_timer
+        }
+        timeout {
+            fatal result FAIL "SSH connection to \"$IP\" failed (timeout)"
+        }
+        eof {
+            fatal result FAIL "SSH connection to \"$IP\" failed (eof)"
+        }
+    }
+}
