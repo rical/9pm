2fca4eb8b1d4c1a0c960ec10f9bfe5b0619328a5
Richard A (rical) <rical@highwind.se>
2014-07-09 21:09:24 +0200

    Split execute code
    Split execution code into "start", "capture" and "finish". Execute works
    the same but the underlying functionalities of execute can now be used
    separately.
    
    The most likely usecase for this is when the user wants to manually
    interact with the command running.
    
    For example:
    
    start "scp foo machine:"
    expect {
    	"password" { send ... }
    }
    set return [finish]
    
    or even
    
    start "scp foo machine:"
    expect {
    	"password" { send ... }
    }
    set output [capture]
    set return [finish]
    
    The checksum stored in the int::shell data structure can be utilized
    with -notransfer in order to get out of a custom expect loop.
    
    set checksum [dict get $int::shell($int::active_shell) "checksum"]
    expect {
    	foo { }
    	-notransfer -re "$checksum (\[0-9]+)\r\n" {
    		# We are done
    	}
    	default { fail... }
    }

--- execute.tcl
+++ execute.tcl
@@ -1,35 +1,95 @@
 package provide 9pm::execute 1.0
 
-proc execute {cmd args} {
-    set out [list]
+proc start {cmd} {
+    if {![info exists int::active_shell]} {
+        fatal int::user_error "You need a spawn to start \"$cmd\""
+    }
+    if {[dict exists $int::shell($int::active_shell) "running"]} {
+        fatal int::user_error "Can't start, shell has command still running"
+    }
     set checksum(start) "[get_rand_str 10][get_rand_int 1000]"
     set checksum(end) "[get_rand_str 10][get_rand_int 1000]"
-    set active FALSE
-
-    upvar ? "code"
-
-    output DEBUG "Executing \"$cmd\" $args"
+    dict set int::shell($int::active_shell) "running" $cmd
+    dict set int::shell($int::active_shell) "checksum" $checksum(end)
 
     expect *
     send "echo $checksum(start); $cmd; echo $checksum(end) \$?\n"
     expect {
-        -re {([^\r\n]+)\r\n} {
+        -timeout 1
+        -re "\r\n$checksum(start)\r\n" {
+            output DEBUG2 "\"$cmd\" started"
+            output DEBUG2 "\"$cmd\" start checksum $checksum(start)"
+            output DEBUG2 "\"$cmd\" end checksum $checksum(end)"
+        }
+        timeout {
+            fatal result FAIL "Timeout starting \"$cmd\""
+        }
+        eof {
+            fatal result FAIL "Got EOF when starting \"$cmd\""
+        }
+    }
+}
+
+proc capture {} {
+    set out [list]
+
+    if {![info exists int::active_shell]} {
+        fatal int::user_error "You need a spawn to caputre output"
+    }
+    if {![dict exists $int::shell($int::active_shell) "running"]} {
+        fatal int::user_error "Can't capture output, nothing running on this shell"
+    }
+
+    set cmd [dict get $int::shell($int::active_shell) "running"]
+    set checksum [dict get $int::shell($int::active_shell) "checksum"]
+
+    output DEBUG2 "\"$cmd\" capturing output unitl checksum $checksum"
+    expect {
+        # We use notransfer so that we leave the checksum for "finnish"
+        -notransfer -re {([^\r\n]+)\r\n} {
             set line $expect_out(0,string)
             set content $expect_out(1,string)
 
-            if [regexp "$checksum(end) (\[0-9]+)\r\n" $line unused code] {
-                output DEBUG "Got $code as returncode for \"$cmd\""
-            } elseif [regexp "$checksum(start)\r\n" $line unused] {
-                output DEBUG "Activating for \"$content\""
-                set active TRUE
-                exp_continue -continue_timer
-            } else {
-                if {$active} {
-                    output DEBUG "Got: \"$content\""
-                    lappend out $content
-                }
-                exp_continue -continue_timer
+            if [regexp "$checksum (\[0-9]+)\r\n" $line unused code] {
+                output DEBUG2 "Capture hit end checksum for \"$cmd\""
+                return $out
             }
+
+            # Now that we know it's not the checksum, we flush it from the buffer
+            expect {
+                -re {[^\r\n]+\r\n} { }
+                default {
+                    fatal int::error "Something went wrong when flusing output from the exp buffer"
+                }
+            }
+
+            lappend out $content
+            output DEBUG "Got: \"$content\""
+            exp_continue -continue_timer
+        }
+        timeout {
+            fatal result FAIL "Timeout while capturing output for \"$cmd\""
+        }
+        eof {
+            fatal result FAIL "Got eof while wating for return code for \"$cmd\""
+        }
+    }
+}
+
+proc finish {} {
+    if {![info exists int::active_shell]} {
+        fatal int::user_error "Can't finish, no active spawn"
+    }
+    if {![dict exists $int::shell($int::active_shell) "running"]} {
+        fatal int::user_error "Can't finish, nothing running on this shell"
+    }
+
+    set cmd [dict get $int::shell($int::active_shell) "running"]
+    set checksum [dict get $int::shell($int::active_shell) "checksum"]
+
+    expect {
+        -re "$checksum (\[0-9]+)\r\n" {
+            set code $expect_out(1,string)
         }
         timeout {
             fatal result FAIL "Timeout waitning for return code for \"$cmd\""
@@ -37,7 +97,25 @@ proc execute {cmd args} {
         eof {
             fatal result FAIL "Got eof while wating for return code for \"$cmd\""
         }
+
     }
+    output DEBUG "Got $code as returncode for \"$cmd\""
+    dict unset int::shell($int::active_shell) "running"
+    dict unset int::shell($int::active_shell) "checksum"
+    return $code
+
+}
+
+proc execute {cmd args} {
+    upvar ? "code"
+
+    output DEBUG "Executing \"$cmd\" $args"
+
+    start $cmd
+    set out [capture]
+    set code [finish]
+
+    output DEBUG "Execution of \"$cmd\" returned $code with [llength $out] lines of output"
 
     if {([llength $args] > 0) && ([lsearch -exact $args $code] < 0)} {
         fatal result FAIL "Got non-expected return code $code for \"$cmd\""
