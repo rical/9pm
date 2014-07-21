bcfe0f5e485e7b158cd20f520b189f9741ac32d6
Richard A (rical) <rical@highwind.se>
2014-07-22 00:49:52 +0200

    Add rudimentary scp utility
    Get or send files using scp. Supports multiple files in both directions
    and utilizes send/finish with expect_after to know when transfer is
    complete.
    
    Examples:
    
    Send /tmp/foo from current machine to /opt on "lightcycle"
    $ scp "to" "lightcycle" "/tmp/foo" "/opt/"
    
    Get /tmp/foo from "lightcycle" and store in /opt
    $ scp "from" "lightcycle" "/tmp/foo" "/opt/"
    
    Send /tmp/foo and /tmp/bar from current machine to /opt on "lightcycle"
    $ scp "to" "lightcycle" [list /tmp/foo /tmp/bar] "/opt/"
    
    Get /tmp/foo and /tmp/bar from "lightcycle" and store them in /opt
    $ scp "from" "lightcycle" [list "/tmp/bar" "/tmp/bar"] "/tmp/"
    
    Get all files from /tmp/ on "lightcycle" and store in ~/tmp
    $ scp "from" "lightcycle" "/tmp/*" "~/tmp/" "-r"
    
    Send all files from /tmp/ to ~/tmp on "lightcyle"
    $ scp "from" "lightcycle" "/tmp/*" "tmp/" "-r"

--- pkgAll.tcl
+++ pkgAll.tcl
@@ -7,5 +7,6 @@ package require 9pm::init 1.0
 package require 9pm::shell 1.0
 package require 9pm::execute 1.0
 package require 9pm::ssh 1.0
+package require 9pm::scp 1.0
 
 package provide 9pm 1.0
---
+++ scp.tcl
@@ -0,0 +1,56 @@
+package provide 9pm::scp 1.0
+
+proc scp {direction node files dest args} {
+    set IP      [get_req_node_info $node SSH_IP]
+    set PROMPT  [get_req_node_info $node PROMPT]
+    set PORT    [get_node_info $node SSH_PORT]
+    set USER    [get_node_info $node SSH_USER]
+    set KEYFILE [get_node_info $node SSH_KEYFILE]
+
+    set opts [dict get $int::rc "ssh_opts"]
+    set cmd "scp $opts"
+
+    if {$PORT != ""} {
+        append cmd " -P $PORT"
+    }
+    if {$KEYFILE != ""} {
+        append cmd " -i $KEYFILE"
+    }
+    if {$args != ""} {
+        append cmd " $args"
+    }
+
+    set host [expr {($USER != "") ? "$USER@$IP" : "$IP"}]
+
+    if {$direction == "from"} {
+        append cmd " $host:\"$files\" $dest"
+    } elseif {$direction == "to"} {
+        append cmd " $files $host:$dest"
+    } else {
+        fatal int::user_error "Unsupported scp direction \"$direction\""
+    }
+
+    output DEBUG "Scp \"$files\" $direction $host"
+
+    start "$cmd"
+    expect {
+        -nocase "password" {
+            send "[get_req_node_info $node SSH_PASS]\n"
+            exp_continue
+        }
+        -re {(\S+)\s+100%} {
+            output DEBUG "File \"$expect_out(1,string)\" transfered $direction $host"
+            exp_continue -continue_timer
+        }
+        timeout {
+            fatal result FAIL "Scp transfer of \"$files\" $direction $host (timeout)"
+        }
+        eof {
+            fatal result FAIL "Scp transfer of \"$files\" $direction $host (eof)"
+        }
+    }
+    set code [finish]
+    if {$code != 0} {
+        fatal result FAIL "Scp transfer $direction $IP (got non-zero return code ($code))"
+    }
+}
