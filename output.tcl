91ece9713bee1de65d506a8a16f8e26556fd72d5
Richard A (rical) <rical@highwind.se>
2014-08-11 23:11:41 +0200

    Fix incorrect GPL version in e6125a (v3 -> v2)
    A copy paste error in e6125a resulted in the wrong GLP version. Again,
    9pm is and has always been intended to be licensed under GPL v2.

--- output.tcl
+++ output.tcl
@@ -2,10 +2,10 @@
 #
 # This file is part of 9pm.
 #
-# 9pm is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# 9pm is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
 #
 # 9pm is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,7 +13,8 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with 9pm.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 package require 9pm::setup
 package provide 9pm::output 1.0
