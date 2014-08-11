e6125af25201477ce4904a82d989e6a4f5c4b2cc
Richard A (rical) <rical@highwind.se>
2014-08-11 19:29:52 +0200

    Add GPL2 information and file headers
    This software is as the README file has always indicated
    but not explicitly stated licensed under GPL2.
    
    I have written this code on my spare time to manage my server park. Now
    that the interest for it is growing I decided to add more explicit
    licensing information.
    
    Also update the README layout to look more like asciidoc and more
    cleanly present the license information.

--- init.tcl
+++ init.tcl
@@ -1,3 +1,20 @@
+# Copyright (C) 2011-2014 Richard Alpe <rical@highwind.se>
+#
+# This file is part of 9pm.
+#
+# 9pm is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# 9pm is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with 9pm.  If not, see <http://www.gnu.org/licenses/>.
+
 package require cmdline
 package require 9pm::output
 package provide 9pm::init 1.0
--- unit_tests/execute_test/execute_test.tcl
+++ unit_tests/execute_test/execute_test.tcl
@@ -1,4 +1,22 @@
 #!/usr/bin/tclsh
+#
+# Copyright (C) 2011-2014 Richard Alpe <rical@highwind.se>
+#
+# This file is part of 9pm.
+#
+# 9pm is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# 9pm is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with 9pm.  If not, see <http://www.gnu.org/licenses/>.
+
 package require 9pm
 
 shell "localhost"
