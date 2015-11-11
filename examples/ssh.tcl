dbea11884121f5cba7d4eeb1e17b1ea8493c7647
Richard A (rical) <rical@highwind.se>
2015-11-11 20:14:41 +0100

    examples: add ssh example

---
+++ examples/ssh.tcl
@@ -0,0 +1,18 @@
+#!/usr/bin/tclsh
+
+# Example of how to open a local shell, ssh into a machine and print its hostname
+# Execute this with $ ./ssh.tcl -c conf.yaml
+
+# Source the 9pm library
+package require 9pm
+
+# Open a shell with a name of our choice, lets call it "foobar"
+9pm::shell::open "foobar"
+
+# Move the active shell (foobar) to the remote machine "machine" present in the configuration file
+9pm::ssh::connect "machine"
+
+# Execute the command "hostname" on the remote machine and capture the output
+set hostname [9pm::cmd::execute "hostname" 0]
+
+puts "Hostname of remote machine: $hostname"
