77f2dd96200c5a8c9f27a7cf038008aa60bf35fa
Richard Alpe <rical@highwind.se>
2019-10-17 20:24:10 +0200

    Add unit test that detects command line pollution
    To catch potential regressions.

---
+++ unit_tests/sanity/noopt.tcl
@@ -0,0 +1,13 @@
+#!/usr/bin/tclsh
+#
+package require 9pm
+
+9pm::output::plan 1
+
+if {[llength $argv] == 0} {
+    9pm::output::ok "No options passed by default"
+} elseif {[lindex $argv 0] == "cmdl-supplied"} {
+    9pm::output::ok "No options passed by default (only \"cmdl-supplied\")"
+} else {
+    9pm::fatal 9pm::output::fail "Command line polluted: \"$argv\""
+}
