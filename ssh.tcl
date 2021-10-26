6530b7b1022879b103884e434635e4887f4d6015
Niklas Söderlund <niklas.soderlund@ragnatech.se>
2021-10-26 16:32:10 +0200

    ssh: Add a new spawn centred around ssh
    In addition to the standard shell spawn add a ssh spawn that directly
    creates a ssh process. This could previously be done by first creating
    a local shell and then using the ssh helper to jump to the next host,
    which still is valid. But for some use-cases it's nicer to spawn the ssh
    process directly for safety.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>

--- ssh.tcl
+++ ssh.tcl
@@ -3,14 +3,41 @@ package provide 9pm::ssh 1.0
 # Wee need Expect TODO: Check if it exists (gracefull error-out)
 package require Expect
 
-
 namespace eval ::9pm::ssh {
-    proc connect {node args} {
+    namespace eval spawn {
+        proc create {node} {
+            set pid [spawn {*}[::9pm::ssh::build_cmd $node]]
+            if {$pid == 0} {
+                ::9pm::fatal ::9pm::output::fail "Failled to connect to spawn for ssh"
+            }
+
+            ::9pm::ssh::login $node $spawn_id
+            send "unset HISTFILE\n"
+
+            return [list $spawn_id $pid]
+        }
+
+        proc open {alias node} {
+            return [::9pm::spawn::open $alias "::9pm::ssh::spawn::create {$node}"]
+        }
+
+        proc close {alias} {
+            return [::9pm::spawn::close $alias]
+        }
+
+        proc push {alias node} {
+            return [::9pm::spawn::push $alias "::9pm::ssh::spawn::create {$node}"]
+        }
+
+        proc pop { } {
+            return [::9pm::spawn::pop]
+        }
+    }
+
+    proc build_cmd {node args} {
         set hostname [::9pm::misc::dict::require $node hostname]
-        set prompt   [::9pm::misc::dict::require $node prompt]
         set port     [::9pm::misc::dict::get $node port]
         set username [::9pm::misc::dict::get $node username]
-        set password [::9pm::misc::dict::get $node password]
         set keyfile  [::9pm::misc::dict::get $node keyfile]
 
         set opts [dict get $::9pm::core::rc "ssh_opts"]
@@ -25,15 +52,22 @@ namespace eval ::9pm::ssh {
         if {$keyfile != ""} {
             append ssh_cmd " -i $keyfile"
         }
-        if {$args != ""} {
+        if {$args != "" && $args != "{}"} {
             append ssh_cmd " $args"
         }
 
+        return $ssh_cmd
+    }
+
+    proc login {node spawnid} {
+        set hostname [::9pm::misc::dict::require $node hostname]
+        set prompt   [::9pm::misc::dict::require $node prompt]
+        set password [::9pm::misc::dict::get $node password]
+
         ::9pm::output::debug "Connecting to \"$hostname\""
 
-        expect *
-        send "$ssh_cmd\n"
         expect {
+            -i $spawnid
             $prompt {
                 ::9pm::output::debug "Connected to \"$hostname\""
             }
@@ -52,5 +86,13 @@ namespace eval ::9pm::ssh {
                 ::9pm::fatal ::9pm::output::fail "SSH connection to \"$hostname\" failed (eof)"
             }
         }
+    }
+
+    proc connect {node args} {
+        global spawn_id
+
+        expect *
+        send "[::9pm::ssh::build_cmd $node $args]\n"
+        ::9pm::ssh::login $node $spawn_id
     }
 }
