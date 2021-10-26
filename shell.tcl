8fa5fde67bb34ad6477962ca6b6202dfbfd41ece
Niklas Söderlund <niklas.soderlund@ragnatech.se>
2021-10-26 15:30:03 +0200

    spawn: Breakout spawn management from shell management
    The mana gent of shells and spawns are two different things, break them
    apart. This paves way for more use-case specific implementations of
    different types of shell implementations that may have different quirks
    then a local /bin/bash shell while still allowing it to coexist with
    other spawns.
    
    There is no functional change in this patch.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>

--- shell.tcl
+++ shell.tcl
@@ -17,84 +17,35 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 package provide 9pm::shell 1.0
+package require 9pm::spawn
 
 namespace eval ::9pm::shell {
-    proc open {alias {shell "/bin/bash -norc"}} {
-        global spawn_id
-        variable data
-        variable active
+    proc create {{shell "/bin/bash -norc"}} {
+        set ::env(HISTFILE) "/dev/null"
 
-        # Write console output to logfile
-        log_file ;#Stop any old logging
-        log_file -a "$::9pm::output::log_path/${alias}_console.log"
+        set pid [spawn {*}$shell]
+        return [list $spawn_id $pid]
+    }
 
-        # Unregister any running command catchers for the previous spawn
-        ::9pm::cmd::int::unreg_exp_after
-
-        if {[info exists data($alias)]} {
-            ::9pm::output::debug "Now looking at shell: $alias"
-            set spawn_id [dict get $data($alias) "spawn_id"]
+    proc open {alias {shell ""}} {
+        if {$shell != ""} {
+            set cmd "::9pm::shell::create {$shell}"
         } else {
-            ::9pm::output::debug "Spawning new shell: \"$alias\""
-            set ::env(HISTFILE) "/dev/null"
-            set pid [spawn {*}$shell]
-
-            if {$pid == 0} {
-                ::9pm::fatal ::9pm::output::error "Failed to spawn shell \"$alias\""
-            }
-
-            dict append data($alias) "spawn_id" $spawn_id
-            dict append data($alias) "pid" $pid
+            set cmd "::9pm::shell::create"
         }
 
-        set active $alias
-        ::9pm::cmd::int::reg_exp_after
-        return TRUE
+        return [::9pm::spawn::open $alias $cmd]
     }
 
     proc close {alias} {
-        variable data
-        variable active
-
-        if {![info exists data($alias)]} {
-            ::9pm::fatal ::9pm::output::user_error "Trying to close shell \"$alias\" that doesn't exist"
-        }
-
-        # Unset active shell if that is the one we are closing
-        if {[info exists active] && ($active == $alias)} {
-            ::9pm::output::debug "Closing active shell: $alias"
-            unset active
-        } else {
-            ::9pm::output::debug "Closing shell: $alias"
-        }
-
-        ::close -i [dict get $data($alias) "spawn_id"]
-
-        # Remove it from internal data structure and stop logging
-        unset data($alias)
-        log_file
+        return [::9pm::spawn::close $alias]
     }
 
     proc push {alias} {
-        variable active
-        variable stack
-
-        if {![info exists active]} {
-            ::9pm::fatal ::9pm::output::user_error "You need to have a shell in order to push it"
-        }
-        lappend stack $active
-        return [::9pm::shell::open $alias]
+        return [::9pm::spawn::push $alias {::9pm::shell::create}]
     }
 
     proc pop { } {
-        variable active
-        variable stack
-
-        if {[llength $stack] == 0} {
-            ::9pm::fatal ::9pm::output::user_error "Can't pop shell, stack is empty"
-        }
-        set alias [lindex $stack end]
-        set stack [lreplace $stack [set stack end] end]
-        return [::9pm::shell::open $alias]
+        return [::9pm::spawn::pop]
     }
 }
