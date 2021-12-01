8dc51a3261f80f52326b932b8d627060487ebc26
Niklas Söderlund <niklas.soderlund@ragnatech.se>
2021-12-02 00:02:29 +0100

    ssh: Spell out the full name of node properties
    Update the node property variable names by spelling them out and in
    lower-case to match the style of other files. This is done to not
    confuse them with environment variables that traditionally are in
    upper-case. There is no functional change.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>

--- ssh.tcl
+++ ssh.tcl
@@ -6,50 +6,50 @@ package require Expect
 
 namespace eval ::9pm::ssh {
     proc connect {node args} {
-        set IP      [::9pm::misc::dict::require $node hostname]
-        set PROMPT  [::9pm::misc::dict::require $node prompt]
-        set PORT    [::9pm::misc::dict::get $node port]
-        set USER    [::9pm::misc::dict::get $node username]
-        set PASS    [::9pm::misc::dict::get $node password]
-        set KEYFILE [::9pm::misc::dict::get $node keyfile]
+        set hostname [::9pm::misc::dict::require $node hostname]
+        set prompt   [::9pm::misc::dict::require $node prompt]
+        set port     [::9pm::misc::dict::get $node port]
+        set username [::9pm::misc::dict::get $node username]
+        set password [::9pm::misc::dict::get $node password]
+        set keyfile  [::9pm::misc::dict::get $node keyfile]
 
         set opts [dict get $::9pm::core::rc "ssh_opts"]
 
-        set ssh_cmd "ssh $opts $IP"
-        if {$USER != ""} {
-            append ssh_cmd " -l $USER"
+        set ssh_cmd "ssh $opts $hostname"
+        if {$username != ""} {
+            append ssh_cmd " -l $username"
         }
-        if {$PORT != ""} {
-            append ssh_cmd " -p $PORT"
+        if {$port != ""} {
+            append ssh_cmd " -p $port"
         }
-        if {$KEYFILE != ""} {
-            append ssh_cmd " -i $KEYFILE"
+        if {$keyfile != ""} {
+            append ssh_cmd " -i $keyfile"
         }
         if {$args != ""} {
             append ssh_cmd " $args"
         }
 
-        ::9pm::output::debug "Connecting to \"$IP\""
+        ::9pm::output::debug "Connecting to \"$hostname\""
 
         expect *
         send "$ssh_cmd\n"
         expect {
-            $PROMPT {
-                ::9pm::output::debug "Connected to \"$IP\""
+            $prompt {
+                ::9pm::output::debug "Connected to \"$hostname\""
             }
             -nocase "password" {
-                if {$PASS == ""} {
+                if {$password == ""} {
                     ::9pm::fatal ::9pm::output::fail \
                         "SSH got password prompt but no password is provided in config"
                 }
-                send "$PASS\n"
+                send "$password\n"
                 exp_continue -continue_timer
             }
             timeout {
-                ::9pm::fatal ::9pm::output::fail "SSH connection to \"$IP\" failed (timeout)"
+                ::9pm::fatal ::9pm::output::fail "SSH connection to \"$hostname\" failed (timeout)"
             }
             eof {
-                ::9pm::fatal ::9pm::output::fail "SSH connection to \"$IP\" failed (eof)"
+                ::9pm::fatal ::9pm::output::fail "SSH connection to \"$hostname\" failed (eof)"
             }
         }
     }
