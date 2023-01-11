00f588d5009e99cde43629cf7e1b9518a1161564
Richard Alpe <rical@highwind.se>
2023-01-11 13:25:37 +0100

    arg: add new argument parsing functionality
    This functions helps a user to parse arguments in a test-case.
    
    All arguments on the format foo:bar (*:*) are removed from $argv when
    calling any of the 9pm::arg functions. This makes them position
    independent and should not break old style [lindex $argv 1] parsing
    (as long as the argument isn't on this format).
    
    Examples:
      9pm::arg::require "foo"
    
      This requires the option "foo" to be passed on the format "foo:bar",
      where "bar" is the value. It's up to the test author to handle the
      value, which is stored in $9pm::arg::foo. If "foo" is not passed
      a fatal fail will be issued.
    
      9pm::arg::optional "foo"
    
      Does the same thing as 9pm::arg::require without throwing a fatal
      fail if "foo" is not set.
    
      9pm::arg::require_or_skip "foo" "bar"
    
      Requires the option "foo" to be "bar", i.e.
      "9pm.py -o 'foo:bar' test.tcl". If it's not passed at all or if its
      value isn't 0, the test will output a 1 test plan and skip the test.
      This can be useful when a user wants to run a test on a specific
      hardware for example. Such as:
      9pm::arg::require_or_skip "hardware" "ppc"
    
    Signed-off-by: Richard Alpe <rical@highwind.se>

--- unit_tests/harness/options.tcl
+++ unit_tests/harness/options.tcl
@@ -21,15 +21,21 @@
 package require 9pm
 namespace path ::9pm
 
-output::plan 3
+output::plan 4
 
-puts "args: $argv"
-output::debug "$argv"
+output::debug "argv before ::arg:: $argv"
+
+9pm::arg::require "mode"
+9pm::arg::optional "optional-negative"
+9pm::arg::optional "optional-positive"
+9pm::arg::require_or_skip "skip-positive" "yes"
+
+output::debug "argv after ::arg:: $argv"
 
 if {[llength $argv] == 2} {
     output::ok "correct number of arguments"
 } else {
-    output::fail "unexpected number of arguments"
+    output::fail "unexpected number of arguments ([llength $argv])"
 }
 
 if {[lindex $argv 0] == "suite-supplied"} {
@@ -43,3 +49,11 @@ if {[lindex $argv 1] == "cmdl-supplied"} {
 } else {
     output::fail "no argument from command line"
 }
+
+if {[info exists 9pm::arg::optional-positive]} {
+    output::ok "got optional argument"
+} else {
+    output::fail "didn't get optional argument"
+}
+
+
