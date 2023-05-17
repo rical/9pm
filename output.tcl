4bace57850b1786670a9308bfcb5624fbecc89fa
Richard Alpe <richard@bit42.se>
2023-05-17 10:38:54 +0200

    Remove GPL header from source code files
    No change to the actual License, the GPLv2 document is in COPYING and
    github recognizes is as GPL v2. The headers where messy and missing
    from many files.
    
    Signed-off-by: Richard Alpe <richard@bit42.se>

--- output.tcl
+++ output.tcl
@@ -1,21 +1,3 @@
-# Copyright (C) 2011-2014 Richard Alpe <rical@highwind.se>
-#
-# This file is part of 9pm.
-#
-# 9pm is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# 9pm is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
 package require 9pm::init
 package require 9pm::helpers
 package provide 9pm::output 1.0
--- unit_tests/helpers/retry_test.tcl
+++ unit_tests/helpers/retry_test.tcl
@@ -1,22 +1,4 @@
 #!/usr/bin/tclsh
-#
-# Copyright (C) 2015 Niklas Söderlund <niso@kth.se>
-#
-# This file is part of 9pm.
-#
-# 9pm is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# 9pm is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 package require 9pm
 namespace path ::9pm
