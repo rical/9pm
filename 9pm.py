rical/9pm

commit 43c46da48f3ffab9f93c116d12518fe682cada88

9pm: add the ability to skip an entire suite
Writing a TAP ok message which contains "skip suite" will tell the
9pm.py harness to skip all subsequent tests in that suite.

This is useful if a test suite isn't applicable on a particular
hardware (or similar).

Signed-off-by: Richard Alpe <rical@highwind.se>

9pm.py
@@ -74,6 +74,7 @@ def execute(args, test):
         ok = re.search('^ok (\d+) -', string)
         not_ok = re.search('^not ok (\d+) -', string)
         skip = re.search('^ok (\d+) # skip', string)
+        skip_suite = re.search('^ok (\d+) # skip suite', string)
 
         if plan:
             cprint(pcolor.purple, '{} {}'.format(stamp, string))
@@ -81,6 +82,11 @@ def execute(args, test):
         elif skip:
             cprint(pcolor.yellow, '{} {}'.format(stamp, string))
             test['executed'] = skip.group(1)
+            skip = True
+        elif skip_suite:
+            cprint(pcolor.yellow, '{} {}'.format(stamp, string))
+            test['executed'] = skip.group(1)
+            skip_suite = True
             skip = True
         elif ok:
             cprint(pcolor.green, '{} {}'.format(stamp, string))
@@ -102,7 +108,7 @@ def execute(args, test):
     if exitcode != 0:
         err = True
 
-    return skip, err
+    return skip_suite, skip, err
 
 def run_onfail(cmdline, test):
     args = []
@@ -127,10 +133,15 @@ def run_test(cmdline, test):
         args.extend(test['options'])
 
     print(pcolor.blue + "\nStarting test", test['name'] + pcolor.reset)
+
+    if test['result'] == "skip":
+        print("{}Skip test {} (suite skip){}" . format(pcolor.yellow, test['name'], pcolor.reset))
+        return True, True, False
+
     if cmdline.debug:
         print("Executing:", [test['case']] + args)
 
-    skip, err = execute(args, test)
+    skip_suite, skip, err = execute(args, test)
 
     if 'plan' not in test:
         print("test error, no plan")
@@ -144,7 +155,7 @@ def run_test(cmdline, test):
         print("test error, not conforming to plan ({}/{})".format(test['executed'], test['plan']))
         err = True
 
-    return skip, err
+    return skip_suite, skip, err
 
 # In this function, we generate an unique name for each case and suite. Both
 # suites and cases can be passed an arbitrary amount of times and the same test
@@ -277,13 +288,13 @@ def probe_suite(data):
 
     data['result'] = "noexec"
 
-def run_suite(cmdline, data):
+def run_suite(cmdline, data, skip_suite):
     skip = False
     err = False
 
     for test in data['suite']:
         if 'suite' in test:
-            subskip, suberr = run_suite(cmdline, test)
+            subskip, suberr = run_suite(cmdline, test, skip_suite)
             if subskip:
                 skip = True
             if suberr:
@@ -299,7 +310,10 @@ def run_suite(cmdline, data):
                 print("error, test case not executable {}".format(test['case']))
                 sys.exit(1)
 
-            subskip, suberr = run_test(cmdline, test)
+            if skip_suite:
+                test['result'] = "skip"
+
+            skip_suite, subskip, suberr = run_test(cmdline, test)
             if suberr:
                 if 'mask' in test and test['mask'] == "fail":
                     print("{}Test failure is masked in suite{}" . format(pcolor.red, pcolor.reset))
@@ -402,7 +416,7 @@ def main():
 
     setup_env(args)
 
-    skip, err = run_suite(args, cmdl)
+    skip, err = run_suite(args, cmdl, False)
     if err:
         cprint(pcolor.red, "\nx Execution")
     elif skip:

arg.tcl
@@ -58,15 +58,23 @@ namespace eval ::9pm::arg {
         _argset $name
     }
 
-    # Needs to be called before non-skip-plan
-    proc require_or_skip {name value} {
+    proc _require_or_skip {skip_func name value} {
         _argset $name
         if {![info exists "::9pm::arg::$name"]} {
-            ::9pm::output::skip_test "Required argument \"$name\" not set"
+            $skip_func "Required argument \"$name\" not set"
             exit
         } elseif {[set ::9pm::arg::$name] != $value} {
-            ::9pm::output::skip_test "Required argument \"$name\" != \"$value\""
+            $skip_func "Required argument \"$name\" != \"$value\""
             exit
         }
     }
+
+    proc require_or_skip {name value} {
+        _require_or_skip "::9pm::output::skip_test" $name $value
+    }
+
+    proc require_or_skip_suite {name value} {
+        _require_or_skip "::9pm::output::skip_suite" $name $value
+    }
+
 }

output.tcl
@@ -130,6 +130,10 @@ namespace eval ::9pm::output {
         exit 0
     }
 
+    proc skip_suite {msg} {
+        skip_test "suite $msg"
+    }
+
     proc error {msg} {
         write "# ERROR:: $msg" RED
         return FALSE

unit_tests/auto.yaml
@@ -13,6 +13,8 @@
   name: "shell"
 - suite: "harness/options.yaml"
   name: "options"
+- suite: "harness/suite_skip.yaml"
+  name: "suite-skip"
 - suite: "harness/onfail.yaml"
   name: "on-fail"
 - suite: "helpers/helpers.yaml"

commit 172fda70ee17b047e182a0f4ea5222e2fb20b899

9pm: Fix regressions related to suite skipping
- Make sure that `skip_suite` is always bound to a value. Otherwise, a
  test producing no output would trigger an exception in 9pm.

- Ensure that three values are always returned from `run_test`. This
  was not done on some error paths.

Fixes: 43c46da ("9pm: add the ability to skip an entire suite")
Signed-off-by: Tobias Waldekranz <tobias@waldekranz.com>

9pm.py
@@ -41,6 +41,7 @@ def cprint(color, *args, **kwargs):
 
 def execute(args, test):
     proc = subprocess.Popen([test['case']] + args, stdout=subprocess.PIPE)
+    skip_suite = False
     skip = False
     err = False
 
@@ -127,11 +128,11 @@ def run_test(cmdline, test):
 
     if 'plan' not in test:
         print("test error, no plan")
-        return False, True
+        return False, False, True
 
     if 'executed' not in test:
         print("test error, no tests executed")
-        return False, True
+        return False, False, True
 
     if test['plan'] != test['executed']:
         print("test error, not conforming to plan ({}/{})".format(test['executed'], test['plan']))