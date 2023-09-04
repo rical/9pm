88bd49dbfe2e7088078f5296cafe8670c0688489
Richard Alpe <richard@bit42.se>
2023-09-04 13:39:34 +0200

    unit_test: allow tap.yaml to be executed from cmdline
    Don't assume it's started from run.sh.
    
    Signed-off-by: Richard Alpe <richard@bit42.se>

--- unit_tests/tap/tap.tcl
+++ unit_tests/tap/tap.tcl
@@ -3,10 +3,21 @@
 package require 9pm
 namespace path ::9pm::
 
-set states [lreplace $argv end end]
 
-output::plan [llength $states]
+set states [list "skip" "ok"]
 
-foreach state $states {
+set plan 0
+foreach state $argv {
+    if {[lsearch $states $state] >= 0} {
+        incr plan
+    }
+}
+
+output::plan $plan
+
+foreach state $argv {
+    if {[lsearch -inline $states $state] < 0} {
+        continue
+    }
     output::$state "This is a $state TAP test point"
 }
